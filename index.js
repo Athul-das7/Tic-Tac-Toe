114c901aa207e61a6b5c4003501ec17f3823b405
Athul-das7 <75476124+Athul-das7@users.noreply.github.com>
2021-09-28 14:39:03 +0530

    Update js

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-let playerTurn;
-let cellElements = document.getElementsByClassName("cell");
-console.log(cellElements);
+const x = "x",
+  o = "o";
+let playerTurn = x;
+let cellElements = document.querySelectorAll("[datacell]");
+const wins = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 cellElements.forEach((cell) => {
   cell.addEventListener("click", addClass);
 });
 
 function addClass(e) {
   let cell = e.target;
-  cell.classList.add("x");
+  cell.classList.add(playerTurn);
+  if (wonGame(playerTurn)) {
+    console.log(playerTurn + " won");
+  }
+  if (playerTurn == x) playerTurn = o;
+  else playerTurn = x;
+}
+
+function wonGame(presentPlayer) {
+  return wins.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(presentPlayer);
+    });
+  });
 }
